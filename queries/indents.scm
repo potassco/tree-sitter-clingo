05efde145ec27fc37447ec25d711cf8c0c2d8b26
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2025-09-27 21:55:36 +0200

    refine indentation queries

--- queries/indents.scm
+++ queries/indents.scm
@@ -1,25 +1,43 @@
-; TODO: support for aligning aggregate elements would be a nice idea as well.
-
-((statement) @indent.align
+; head :- a,
+;         b.
+; head :-
+;   a,
+;   b.
+;
+; We use the error node here to also indent partial rules.
+([(ERROR) (statement)] @indent.align
   (#contains? @indent.align ":-")
   (#set! indent.open_delimiter ":-")
   (#set! indent.close_delimiter ".")
   (#set! indent.increment 3))
 
-((ERROR) @indent.align
-  (#contains? @indent.align ":-")
-  (#set! indent.open_delimiter ":-")
+; similar to rule but for weak constraints
+([(ERROR) (statement)] @indent.align
+  (#contains? @indent.align ":~")
+  (#set! indent.open_delimiter ":~")
   (#set! indent.close_delimiter ".")
   (#set! indent.increment 3))
 
-((statement) @indent.align
+; similar to rule but for statements with a condition
+([(ERROR) (statement)] @indent.align
   (#not-contains? @indent.align ":-")
+  (#not-contains? @indent.align ":~")
   (#set! indent.open_delimiter ":")
   (#set! indent.close_delimiter ".")
   (#set! indent.increment 2))
 
-((ERROR) @indent.align
-  (#not-contains? @indent.align ":-")
-  (#set! indent.open_delimiter ":")
-  (#set! indent.close_delimiter ".")
+; #minimize { a: a; 
+;             b: b; 
+;             c: c;
+;             d: d }.
+; Also applies to nested aggregates and theory atoms. We do not treat the error
+; node here. An editor that ensures balanced braces should help.
+([(minimize) (maximize) (set_aggregate) (body_aggregate) (head_aggregate) (theory_atom)] @indent.align
+  (#set! indent.open_delimiter "{")
+  (#set! indent.close_delimiter "}")
   (#set! indent.increment 2))
+
+; indent conditional literals
+((conditional_literal) @indent.align
+                       (#set! indent.open_delimiter ":") 
+                       (#set! indent.increment 2))
